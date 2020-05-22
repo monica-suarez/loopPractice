252bdb9d336709ac163590c3b58d3e60b8d7d881
monica-suarez <shutterbugmo@gmail.com>
2020-05-22 12:23:48 -0500

    2 completed

---
+++ main.js
@@ -0,0 +1,58 @@
+//console.log 1-1000
+const thousand = () => {
+    let number = 0;
+
+    do {
+        number += 1;
+        console.log(number)
+    } while (number < 1000)
+}
+
+thousand();
+
+let person = {firstName: "Jane",
+    lastName: "Doe",
+    birthDate: "Jan 5, 1925",
+    gender: "female"
+};
+
+const person = {firstName: "Peter", lastName: "Wisser", age:25};
+const loopOverObject = (obj) => {
+  let text = "";
+  let x;
+
+  for (x in obj) {
+    text += obj[x] + " ";
+  }
+  return text
+}
+loopOverObject(person)
+
+let arrayOfPersons = [
+  {
+  firstName: "Jane",
+  lastName: "Doe",
+  birthDate: "Jan 5, 1925",
+  gender: "female"
+},
+{
+  firstName: "John",
+  lastName: "Smith",
+  birthDate: "Nov 15, 1935",
+  gender: "male"
+},
+{
+  firstName: "Jessie",
+  lastName: "Perez",
+  birthDate: "Feb 5, 1872",
+  gender: "female"
+},
+{
+  firstName: "Billy",
+  lastName: "Kid",
+  birthDate: "Jul 23, 2018",
+  gender: "male"
+}
+]
+const newArray = arrayOfPersons.map(elem => elem.firstName)
+console.log(newArray)
